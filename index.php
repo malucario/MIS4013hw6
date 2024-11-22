429c2c3eed9cafd225b5e7aa066bdd2dda59786f
malucario <122913162+malucario@users.noreply.github.com>
2024-11-21 21:53:18 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,11 @@
 
+<?php
+Include "view-header.php";
+?>
+
+<h1>Learning JavaScript Libraries!</h1>
+ <p>Hello! Welcome to my homework six. Click on one of the tabs in the navbar to view some cool stuff I've done with my imported libraries.</p>
+
+ <?php
+Include "view-footer.php";
+?>
